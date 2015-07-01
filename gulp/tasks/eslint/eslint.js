dcddbb06b849b54675be52db475d2d453c80708a
kiki le singe <aalbertini95@gmail.com>
2015-07-01 13:41:48 +0200

    refactor(aslant): uses gulp load plugins

--- gulp/tasks/eslint/eslint.js
+++ gulp/tasks/eslint/eslint.js
@@ -8,12 +8,11 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var eslint = require('gulp-eslint');
 var config = require('./config').eslint;
 
 gulp.task('lint', function () {
   return gulp.src(config.src)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError());
 });
