fc94fdecd1ebaa0e0f88d4df82e93dacc6f0605a
kiki le singe <aalbertini95@gmail.com>
2015-07-03 14:57:20 +0200

    refactor(app): restructure the html of app

--- app/scripts/components/app.js
+++ app/scripts/components/app.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Link, RouteHandler} from 'react-router';
+import Nav from './nav';
+import {RouteHandler} from 'react-router';
 
 let App = React.createClass({
   render() {
     return (
-      <div className="app">
-        <h1>App</h1>
-        <Link to="home">Home</Link>
-        <Link to="hello">Hello</Link>
-        <Link to="about">About</Link>
-        <RouteHandler/>
+      <div>
+        <header id="header" style={{background: 'lightcoral'}}>
+          <Nav />
+        </header>
+        <div className="content" style={{background: 'lightblue'}}>
+          <RouteHandler />
+        </div>
+        <footer style={{background: 'lightgreen'}}>Footer</footer>
       </div>
     );
   }
