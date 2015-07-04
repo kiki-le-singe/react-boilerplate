cb36c70decf8c0e276fba4c49244ed5cffebac28
kiki le singe <aalbertini95@gmail.com>
2015-07-04 16:02:25 +0200

    docs(index): moves issues in github

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -13,11 +13,6 @@
 // Docs:
 // - https://facebook.github.io/react/docs/reusable-components.html
 
-// Issues:
-// - Uses es6 classes?
-// - Renames script.dev.js to bundle.js?
-// - Uses marked library for markdown ? (@see https://facebook.github.io/react/docs/tutorial.html#adding-markdown)
-
 import $ from 'jquery';
 import React from 'react';
 import Router from 'react-router';
