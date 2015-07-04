78d73ff4baf86da7972045b96c4be146f1e6b83e
kiki le singe <aalbertini95@gmail.com>
2015-07-04 11:19:46 +0200

    feat(toolForm): add toolForm component

---
+++ app/scripts/components/tools/toolForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let ToolForm = React.createClass({
+  render() {
+    return (
+      <div className="tool-form">
+        Hello, world! I am a ToolForm.
+      </div>
+    );
+  }
+});
+
+export default ToolForm;
