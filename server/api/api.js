4268dd4588b8775cb880b77bcef81e9681025ad4
kiki le singe <aalbertini95@gmail.com>
2015-07-07 22:35:22 +0200

    feat(api): add logo attribute

--- server/api/api.js
+++ server/api/api.js
@@ -1,54 +1,67 @@
 exports.tools = [
   {
-    name: 'HTML5 Boilerplate',
-    url: 'http://www.w3.org/TR/html5/'
+    name: 'HTML5',
+    url: 'http://www.w3.org/TR/html5/',
+    logo: '/images/HTML5.png'
   },
   {
     name: 'Foundation',
-    url: 'http://foundation.zurb.com/'
+    url: 'http://foundation.zurb.com/',
+    logo: '/images/foundation.png'
   },
   {
     name: 'jQuery',
-    url: 'http://jquery.com/'
+    url: 'http://jquery.com/',
+    logo: '/images/jquery.png'
   },
   {
     name: 'React',
-    url: 'https://facebook.github.io/react/index.html'
+    url: 'https://facebook.github.io/react/index.html',
+    logo: '/images/react.png'
   },
   {
     name: 'Babel',
-    url: 'https://babeljs.io/'
+    url: 'https://babeljs.io/',
+    logo: '/images/babel.png'
   },
   {
     name: 'Lo-Dash',
-    url: 'http://lodash.com/'
+    url: 'http://lodash.com/',
+    logo: '/images/lodash.png'
   },
   {
     name: 'browserify',
-    url: 'http://browserify.org/'
+    url: 'http://browserify.org/',
+    logo: null
   },
   {
     name: 'Browsersync',
-    url: 'http://www.browsersync.io/'
+    url: 'http://www.browsersync.io/',
+    logo: null
   },
   {
     name: 'Express',
-    url: 'https://www.npmjs.org/package/express'
+    url: 'https://www.npmjs.org/package/express',
+    logo: null
   },
   {
     name: 'KARMA',
-    url: 'http://karma-runner.github.io/'
+    url: 'http://karma-runner.github.io/',
+    logo: '/images/karma.png'
   },
   {
     name: 'Mocha',
-    url: 'http://visionmedia.github.io/mocha/'
+    url: 'http://visionmedia.github.io/mocha/',
+    logo: '/images/mocha.png'
   },
   {
     name: 'Sinon.JS',
-    url: 'http://sinonjs.org/'
+    url: 'http://sinonjs.org/',
+    logo: null
   },
   {
     name: 'chaijs',
-    url: 'http://chaijs.com/'
+    url: 'http://chaijs.com/',
+    logo: '/images/chaijs.png'
   }
 ];
