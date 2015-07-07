c45d528ad2b0c26944fa25b47133466f751d8d68
kiki le singe <aalbertini95@gmail.com>
2015-07-07 22:52:28 +0200

    refactor(toolBoxWrapper): moves the logic in ToolBoxWrapper component

--- app/scripts/components/tools/toolBoxWrapper.js
+++ app/scripts/components/tools/toolBoxWrapper.js
@@ -1,11 +1,37 @@
+import $ from 'jquery';
 import React from 'react';
 import ToolBox from './toolBox';
 
 let ToolBoxWrapper = React.createClass({
+  // componentDidMount is a method called automatically by React when a component is rendered
+  componentDidMount() {
+    this.loadToolsFromServer();
+  },
+
+  // getInitialState() executes exactly once during the lifecycle of the component
+  // and sets up the initial state of the component.
+  // When the state updates, the component re-renders itself.
+  // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
+  getInitialState() {
+    return {data: []};
+  },
+
   render() {
-    return (
-      <ToolBox url="api/tools" />
-    );
+    return <ToolBox data={this.state.data} />;
+  },
+
+  loadToolsFromServer() {
+    $.ajax({
+      url: 'api/tools',
+      dataType: 'json',
+      cache: false,
+      success: (data) => {
+        this.setState({data: data});
+      },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString()); // eslint-disable-line
+      }
+    });
   }
 });
 
