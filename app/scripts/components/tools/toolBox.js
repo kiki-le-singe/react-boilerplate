68e2c46e007b40e53cf8970503c40c8f29021a6f
kiki le singe <aalbertini95@gmail.com>
2015-07-07 22:52:13 +0200

    refactor(toolBox): removes all logic

--- app/scripts/components/tools/toolBox.js
+++ app/scripts/components/tools/toolBox.js
@@ -1,32 +1,20 @@
-import $ from 'jquery';
 import React from 'react';
 import Tools from './tools';
 import ToolForm from './toolForm';
 
 let ToolBox = React.createClass({
   propTypes: {
-    url: React.PropTypes.string.isRequired
-  },
-
-  // componentDidMount is a method called automatically by React when a component is rendered
-  componentDidMount() {
-    this.loadToolsFromServer();
-  },
-
-  // getInitialState() executes exactly once during the lifecycle of the component
-  // and sets up the initial state of the component.
-  // When the state updates, the component re-renders itself.
-  // @see https://facebook.github.io/react/docs/tutorial.html#reactive-state
-  getInitialState() {
-    return {data: []};
+    data: React.PropTypes.array.isRequired
   },
 
   render() {
-    if (this.state.data.length) {
+    let {data} = this.props;
+
+    if (data.length) {
       return (
         <div className="tool-box">
           <h1>Tools</h1>
-          <Tools data={this.state.data} />
+          <Tools data={data} />
           <ToolForm />
         </div>
       );
@@ -37,20 +25,6 @@ let ToolBox = React.createClass({
         <p>tools.empty</p>
       </div>
     );
-  },
-
-  loadToolsFromServer() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: (data) => {
-        this.setState({data: data});
-      },
-      error: (xhr, status, err) => {
-        console.error(this.props.url, status, err.toString()); // eslint-disable-line
-      }
-    });
   }
 });
 
