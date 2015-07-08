49424c850c18f4fc5c90857f0677fa2f74e87b59
kiki le singe <aalbertini95@gmail.com>
2015-07-08 23:16:12 +0200

    refactor(babel): uses watchify with browserify

--- gulp/tasks/babel/babel.js
+++ gulp/tasks/babel/babel.js
@@ -7,6 +7,10 @@
 // https://www.npmjs.com/package/gulp-sourcemaps
   // http://stackoverflow.com/questions/28087674/gulp-browserify-6to5-source-maps
 // https://www.npmjs.com/package/gulp-load-plugins
+// https://www.npmjs.com/package/watchify
+  // https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+// https://www.npmjs.com/package/gutil
+// https://www.npmjs.com/package/lodash
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
@@ -14,20 +18,34 @@ var sourcemaps = require('gulp-sourcemaps');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var watchify = require('watchify');
+var _ = require('lodash');
 var browserify = require('browserify');
 var config = require('./config').babel;
 
-gulp.task('babelify:dev', function() {
-  browserify({entries: config.entries, debug: true})
-    .transform(babelify)
-    .bundle()
+// add custom browserify options here
+var customOpts = {
+  entries: config.entries,
+  debug: true
+};
+var opts = _.assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+b.transform(babelify);
+
+var bundle = function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source(config.dev.source))
-
-    .pipe(buffer()) // Useful?
-
-    .pipe(sourcemaps.init())
-    .pipe($.uglify())
-    .pipe(sourcemaps.write('.')) // writes .map file
-
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    .pipe(sourcemaps.write('./')) // writes .map file
     .pipe(gulp.dest(config.dev.dest));
-});
+};
+
+gulp.task('babelify:dev', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
