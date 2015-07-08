d12bf65ed43d10e7171c97c0c2d8eb404de2d1b8
kiki le singe <aalbertini95@gmail.com>
2015-07-08 23:37:59 +0200

    refactor(watch): removes useless comments

--- gulp/tasks/watch/config.js
+++ gulp/tasks/watch/config.js
@@ -4,7 +4,5 @@ module.exports = {
   watch: {
     js: ['app/scripts/**/*.js', '!app/scripts/bundle.js'],
     scss: 'app/styles/scss/**/*.scss'
-    // ,
-    // hbs: ['app/scripts/templates/**/*.hbs', 'app/scripts/modules/**/templates/**/*.hbs']
   }
 };
--- gulp/tasks/watch/watch.js
+++ gulp/tasks/watch/watch.js
@@ -1,22 +1,13 @@
 'use strict';
 
-// https://www.npmjs.com/package/run-sequence
-
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
 var config = require('./config').watch;
 
-// gulp.task('watch', ['css-watch', 'hbs-watch']);
+gulp.task('watch', ['css-watch', 'js-watch']);
 
 gulp.task('css-watch', function () {
   gulp.watch(config.scss, ['sass:dev']);
 });
-
-// gulp.task('hbs-watch', function () {
-//   gulp.watch(config.hbs, ['handlebars']);
-// });
-
-gulp.task('watch', ['css-watch', 'js-watch']);
 
 gulp.task('js-watch', function () {
   gulp.watch(config.js, ['scripts:dev']);
