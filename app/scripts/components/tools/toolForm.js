a318b9183086e9f0b1a15a604eeeb881da86d57c
kiki le singe <aalbertini95@gmail.com>
2015-07-12 18:02:04 +0200

    feat(toolForm): prepare toolForm component. Work in progress...

--- app/scripts/components/tools/toolForm.js
+++ app/scripts/components/tools/toolForm.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class ToolForm extends React.Component {
+  constructor() {
+    super();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
     return (
-      <div className="tool-form">
-        Hello, world! I am a ToolForm.
-      </div>
+      <form className="tool-form" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Title" ref="title" />
+        <input type="submit" value="Post" />
+      </form>
     );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let title = React.findDOMNode(this.refs.title).value.trim();
+    if (!title) {
+      return;
+    }
+
+    // TODO: send request to the server
+    React.findDOMNode(this.refs.title).value = '';
   }
 }
 
