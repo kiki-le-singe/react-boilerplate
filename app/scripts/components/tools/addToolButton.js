baa6cd38c7797e2d277dcb7814a6c17eedd06a37
kiki le singe <aalbertini95@gmail.com>
2015-07-12 17:39:31 +0200

    refactor(addToolButton): bind handleClick method to the init

--- app/scripts/components/tools/addToolButton.js
+++ app/scripts/components/tools/addToolButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class AddToolButton extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
     return (
-      <a className="add-tool-btn" onClick={this.handleClick.bind(this)}>
+      <a className="add-tool-btn" onClick={this.handleClick}>
         <i className="fi-plus"></i>
       </a>
     );
