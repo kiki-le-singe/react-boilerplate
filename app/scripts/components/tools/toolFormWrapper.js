2c1d110e7850e00794fca1b0a661e5554028b110
kiki le singe <aalbertini95@gmail.com>
2015-07-15 01:44:10 +0200

    feat(toolFormWrapper): add ToolFormWrapper component

---
+++ app/scripts/components/tools/toolFormWrapper.js
@@ -0,0 +1,47 @@
+import React from 'react/addons';
+import AddToolButton from './addToolButton';
+import ToolForm from './toolForm';
+
+let {Component, PropTypes} = React;
+
+// https://facebook.github.io/react/docs/animation.html#getting-started
+let TransitionGroup = React.addons.TransitionGroup;
+
+class ToolFormWrapper extends Component {
+  constructor() {
+    super();
+
+    this.state = {clicked: false};
+
+    // Autobinding/No Autobinding:
+    // - https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
+    // - https://facebook.github.io/react/docs/reusable-components.html#no-autobinding
+    this.handleAddToolButtonClick = this.handleAddToolButtonClick.bind(this);
+  }
+
+  handleAddToolButtonClick() {
+    this.setState({clicked: !this.state.clicked});
+  }
+
+  render() {
+    let form;
+    if (this.state.clicked) {
+      form = (<ToolForm onToolSubmit={this.props.onToolSubmit} key={1} />);
+    }
+
+    return (
+      <div id="tool-form-wrapper">
+        <TransitionGroup component="div">
+          {form}
+        </TransitionGroup>
+        <AddToolButton onAddToolButtonClick={this.handleAddToolButtonClick} />
+      </div>
+    );
+  }
+}
+
+ToolFormWrapper.propTypes = {
+  onToolSubmit: PropTypes.func.isRequired
+};
+
+export default ToolFormWrapper;
