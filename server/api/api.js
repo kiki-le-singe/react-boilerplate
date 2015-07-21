3e322a2c3cf2e5afe56442b422bbe780b85b563a
kiki le singe <aalbertini95@gmail.com>
2015-07-21 17:43:48 +0200

    refactor(api): update api

--- server/api/api.js
+++ server/api/api.js
@@ -1,67 +1,92 @@
 exports.tools = [
   {
     name: 'HTML5',
+    text: '5th major revision of the core language of the World Wide Web: the Hypertext Markup Language (HTML).',
     url: 'http://www.w3.org/TR/html5/',
-    logo: '/images/HTML5.png'
+    logo: '/images/logos/html5.svg'
   },
   {
-    name: 'Foundation',
-    url: 'http://foundation.zurb.com/',
-    logo: '/images/foundation.png'
+    name: 'Bourbon',
+    text: 'A simple and lightweight mixin library for Sass.',
+    url: 'http://bourbon.io/',
+    logo: '/images/logos/bourbon.svg'
+  },
+  {
+    name: 'Bourbon NEAT',
+    text: 'A lightweight semantic grid framework for Sass and Bourbon.',
+    url: 'http://neat.bourbon.io/',
+    logo: '/images/logos/bourbon-neat.svg'
   },
   {
     name: 'jQuery',
+    text: 'jQuery is a fast, small, and feature-rich JavaScript library.',
     url: 'http://jquery.com/',
-    logo: '/images/jquery.png'
+    logo: '/images/logos/jquery.svg'
   },
   {
     name: 'React',
+    text: 'A javascript library for building user interfaces.',
     url: 'https://facebook.github.io/react/index.html',
-    logo: '/images/react.png'
+    logo: '/images/logos/react.svg'
+  },
+  {
+    name: 'Material UI',
+    text: 'A Set of React Components that Implement Google\'s Material Design.',
+    url: 'http://material-ui.com/',
+    logo: '/images/logos/material-ui.svg'
   },
   {
     name: 'Babel',
+    text: 'Babel is a JavaScript compiler. Use next generation JavaScript, today.',
     url: 'https://babeljs.io/',
-    logo: '/images/babel.png'
+    logo: '/images/logos/babel.svg'
   },
   {
     name: 'Lo-Dash',
+    text: 'A JavaScript utility library delivering consistency, modularity, performance, & extras.',
     url: 'http://lodash.com/',
-    logo: '/images/lodash.png'
+    logo: '/images/logos/lodash.svg'
   },
   {
     name: 'browserify',
+    text: 'Browserify lets you require(\'modules\') in the browser by bundling up all of your dependencies.',
     url: 'http://browserify.org/',
-    logo: null
+    logo: '/images/logos/browserify.svg'
   },
   {
     name: 'Browsersync',
+    text: 'Browsersync makes your tweaking and testing faster by synchronising file changes and interactions across multiple devices. It’s wicked-fast and totally free.',
     url: 'http://www.browsersync.io/',
     logo: null
   },
   {
     name: 'Express',
+    text: 'Fast, unopinionated, minimalist web framework for Node.js',
     url: 'https://www.npmjs.org/package/express',
-    logo: null
+    logo: '/images/logos/express.svg'
   },
   {
     name: 'KARMA',
+    text: 'The main goal for Karma is to bring a productive testing environment to developers.',
     url: 'http://karma-runner.github.io/',
-    logo: '/images/karma.png'
+    logo: '/images/logos/karma.svg'
   },
   {
     name: 'Mocha',
+    text: 'Mocha is a feature-rich JavaScript test framework running on Node.js and the browser, making asynchronous testing simple and fun.',
     url: 'http://visionmedia.github.io/mocha/',
-    logo: '/images/mocha.png'
+    logo: '/images/logos/mocha.svg'
   },
   {
     name: 'Sinon.JS',
+    text: 'Standalone test spies, stubs and mocks for JavaScript. No dependencies, works with any unit testing framework.',
     url: 'http://sinonjs.org/',
     logo: null
   },
   {
     name: 'chaijs',
+    text: 'Chai is a BDD / TDD assertion library for node and the browser that can be delightfully paired with any javascript testing framework.',
     url: 'http://chaijs.com/',
-    logo: '/images/chaijs.png'
+    logo: '/images/logos/chai.svg'
   }
 ];
